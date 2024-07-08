03966a65579bfd9f1b6b5adc820e9e40d610052b
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-08 18:21:34 +0300

    initial commit

---
+++ arrays/todo.js
@@ -0,0 +1,13 @@
+const todo=[]
+
+//lets push 5 things into here
+todo.push('go to gym')
+todo.push('rest')
+todo.push('take a shower')
+todo.push('eat break fast')
+todo.push('wear a suit')
+
+
+//console.log(todo)
+console.log(`'you have ${todo.length} of todo's`)
+console.log(`${todo[0]} -> ${todo[todo.length-2]} `)
