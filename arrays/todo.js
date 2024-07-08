27ab10cb1103b2f66cc3567ee5d1e84e730798be
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-08 18:49:07 +0300

    used splice and shift

--- arrays/todo.js
+++ arrays/todo.js
@@ -9,5 +9,19 @@ todo.push('wear a suit')
 
 
 //console.log(todo)
+
+// console.log(`${todo[0]} -> ${todo[todo.length-2]} `)
+
+//removes the 3rd element
+todo.splice(2,1,)
+
+//add a new element to the end
+todo.push('go to work')
+
+//remove the first element from the array
+todo.shift()
+
+//display
 console.log(`'you have ${todo.length} of todo's`)
-console.log(`${todo[0]} -> ${todo[todo.length-2]} `)
+
+console.log(todo)
