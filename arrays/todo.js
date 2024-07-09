05314d9871eeceb0ab5874ffe5c6b7d91b22d9ea
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-09 11:11:13 +0300

    made a todo with objects as element arrays

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,38 +1,33 @@
-const todo=[]
+//created an object 
+let todo=[
+    {
+        text:"order food",
+        completed:false
+    },
+    {
+        text:'meet up with friends',
+        completed:false
+    },
+    {
+        text:'go out for walk',
+        completed:true
+    },
+    {
+        text:'morning exercise',
+        completed:false
+    }
+]
 
-//lets push 5 things into here
-todo.push('go to gym')
-todo.push('rest')
-todo.push('take a shower')
-todo.push('eat break fast')
-todo.push('wear a suit')
-
-
-//console.log(todo)
-
-// console.log(`${todo[0]} -> ${todo[todo.length-2]} `)
-
-//removes the 3rd element
-todo.splice(2,1,)
-
-//add a new element to the end
-todo.push('go to work')
-
-//remove the first element from the array
-todo.shift()
-
-//display
-console.log(`'you have ${todo.length} of todo's`)
-
-//console.log(todo)
-//lets print out the output lists
-
-// todo.forEach(function(item,index){
-//     console.log(`${index+1} --> ${item}`)
-// })
-
-//lets implement the looping using 'for'
-
-for(let count=0;count<todo.length;count++){
-    console.log(`${count+1}.${todo[count]}`)
+//lets create afunction that removes values based on text value
+const removeobj=function(todo,text){
+    const index= todo.findIndex(function(item){
+        return item.text.toLowerCase()===text.toLowerCase()
+    })
+    if(index>-1){
+        todo.splice(index,1)
+    }
+ 
 }
+
+removeobj(todo,"order food!!!!!")
+console.log(todo)
