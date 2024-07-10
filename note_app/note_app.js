b9247e1f81a1d8cf34586bb9ff6a711a969a64f1
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-10 12:07:07 +0300

    initial commit

---
+++ note_app/note_app.js
@@ -0,0 +1,11 @@
+//lets use DOM to manipulate the html document
+
+//for searching and removing the first found element
+const p=document.querySelector('p')
+//p.remove()
+
+//for searching and removing all the elements
+const ps=document.querySelectorAll('p')   //but this return a list of all available
+ps.forEach(function(item){
+    item.remove()
+})
