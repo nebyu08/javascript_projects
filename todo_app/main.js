4c485801147fe5c99ec8e13ed9fd41a111f678e0
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-11 17:57:14 +0300

    added forum

--- todo_app/main.js
+++ todo_app/main.js
@@ -20,6 +20,8 @@ let todo=[
       completed:false
    }
 ]
+//lets show the total
+
 //variable that we are going to search over
 const todoSearch={
     'todos':''
@@ -29,8 +31,18 @@ const searchTodo=function(todos,search){
    const filtered= todos.filter(function(todo){
         return todo.text.toLowerCase().includes(search.todos.toLowerCase())
     })
+    
+    const Incomplete=filtered.filter(function(item){
+        return !item.completed
+    })
+
     //lets clrear element
     document.querySelector('#todo').innerHTML=' '
+
+    //print the total number of element
+    const header=document.createElement('h2')
+    header.textContent=`total number of tod left is ${Incomplete.length}`
+    document.querySelector('#todo').appendChild(header)
 
     //create element 
     filtered.forEach(function(todo){
@@ -38,22 +50,25 @@ const searchTodo=function(todos,search){
         ps.textContent=todo.text
         document.querySelector('#todo').appendChild(ps)
     })
-}
+
+    }
+
 
 searchTodo(todo,todoSearch)
-
-document.querySelector('#add_todo').addEventListener('click',function(){
-   console.log('adding new todo')
-
-})
-
-//adding event listener to the input
-document.querySelector('#adding_todo').addEventListener('input',function(e){
-    console.log(e.target.value)
-})
 
 document.querySelector('#search_todo').addEventListener('input',function(e){
     const tempVariable=e.target.value
     todoSearch.todos=tempVariable
     searchTodo(todo,todoSearch)
+})
+
+document.querySelector("#id_form").addEventListener('submit',function(e){
+    e.preventDefault()
+    const text=e.target.elements.todo_form.value
+    console.log(text)
+    //lets add the value into the todo
+    todo.push({text:text,completed:false})
+    //lets render what we have so far in our todo
+    searchTodo(todo,todoSearch)
+    e.target.elements.todo_form.value=''
 })
