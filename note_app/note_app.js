cb27aceb7f31e2df35bb9cc21ba52af25ec7d552
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-11 11:49:39 +0300

    added search and print result

--- note_app/note_app.js
+++ note_app/note_app.js
@@ -13,6 +13,30 @@ const note_obj=[
     }
 ]
 
+//lets create variable that can be used for searching
+const filterSearch={
+    'textSearch':''
+}
+
+const filterNote=function(notes,search){
+    const filtered = notes.filter(function(note){
+        return note.title.toLowerCase().includes(search.textSearch.toLowerCase())
+    })
+    
+    //lets clear the previous element search result
+    document.querySelector('#notes').innerHTML=''
+    
+    //lets print values in the paragraph section 
+   filtered.forEach(function(note){
+    const newP=document.createElement('p')
+    newP.textContent=note.title
+    document.querySelector('#notes').appendChild(newP)
+   })
+}
+
+//somthing shows up before user interacts with the page
+filterNote(note_obj,filterSearch)
+
 document.querySelector('#create_note').addEventListener('click',function(e){
   e.target.textContent='button clicked'
 })
@@ -21,4 +45,13 @@ document.querySelector('#remove_note').addEventListener('click',function(){
    document.querySelectorAll('.note').forEach(function(note){
     note.remove()
    })
+})
+
+//handle input
+document.querySelector('#input_id').addEventListener('input',function(e){
+    filterSearch.textSearch= e.target.value
+    //called when user interacts with the page
+    filterNote(note_obj,filterSearch)
+
+
 })
