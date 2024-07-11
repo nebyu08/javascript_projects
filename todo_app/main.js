ba67d9a4215a4a3fef186b948bfe55ce9a743c2c
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-11 13:11:55 +0300

    made it search todos and display them

--- todo_app/main.js
+++ todo_app/main.js
@@ -20,8 +20,40 @@ let todo=[
       completed:false
    }
 ]
+//variable that we are going to search over
+const todoSearch={
+    'todos':''
+}
+
+const searchTodo=function(todos,search){
+   const filtered= todos.filter(function(todo){
+        return todo.text.toLowerCase().includes(search.todos.toLowerCase())
+    })
+    //lets clrear element
+    document.querySelector('#todo').innerHTML=' '
+
+    //create element 
+    filtered.forEach(function(todo){
+        const ps=document.createElement('p')
+        ps.textContent=todo.text
+        document.querySelector('#todo').appendChild(ps)
+    })
+}
+
+searchTodo(todo,todoSearch)
 
 document.querySelector('#add_todo').addEventListener('click',function(){
    console.log('adding new todo')
 
+})
+
+//adding event listener to the input
+document.querySelector('#adding_todo').addEventListener('input',function(e){
+    console.log(e.target.value)
+})
+
+document.querySelector('#search_todo').addEventListener('input',function(e){
+    const tempVariable=e.target.value
+    todoSearch.todos=tempVariable
+    searchTodo(todo,todoSearch)
 })
