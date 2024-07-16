8642dcd270990b6b697e4b4d04f991b751de9b62
nebiyuyouhannes08@gmail.com <nebiyuyouhannes08@gmail.com>
2024-07-16 10:22:12 +0300

    manipluating Date

--- note_app/note_edit.js
+++ note_app/note_edit.js
@@ -44,6 +44,31 @@ window.addEventListener('storage',function(e){
         bodyElement.value=note.body
         
     }
-
-  
 })
+
+// const now=new Date()
+// const timestamp=now.getTime()
+
+// const varDate=new Date(timestamp)
+
+// console.log(`year ${varDate.getFullYear()}: Month ${varDate.getMonth()} and day is ${varDate.getDate()}`)
+
+//lets create 2 dates and compare them
+const Date1=new Date('march 23 1980')
+const Date2=new Date()
+
+const timestamp1=Date1.getTime()
+const timestamp2=Date2.getTime()
+
+console.log(timestamp1)
+console.log(timestamp2)
+
+if(timestamp1<timestamp2){
+    console.log(`the first timestamp is the greater one:${Date1.toString()}`)
+}
+else if(timestamp2>timestamp1){
+    console.log(`the second timestamp is the greater one ${Date2.toString()}`)
+}
+else{
+    console.log('they are equal')
+}
